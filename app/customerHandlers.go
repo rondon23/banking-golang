9b78d6fbe0a584c6f50c0060f6242964a9318961
rondon23 <brunorondon21@gmail.com>
2022-06-01 08:16:44 -0400

    error handling part 2

--- app/customerHandlers.go
+++ app/customerHandlers.go
@@ -4,17 +4,10 @@ import (
 	"banking/service"
 	"encoding/json"
 	"encoding/xml"
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
-
-type Customer struct {
-	Name    string `json:"full_name" xml:"name"`
-	City    string `json:"city" xml:"city"`
-	ZipCode string `json:"zip_code" xml:"zipcode"`
-}
 
 type CustomerHandlers struct {
 	service service.CustomerService
@@ -39,15 +32,21 @@ func (ch *CustomerHandlers) getCustomer(w http.ResponseWriter, r *http.Request) 
 
 	customer, err := ch.service.GetCustomer(id)
 	if err != nil {
-		w.WriteHeader(err.Code)
-		fmt.Fprint(w, err.Message)
+		writeResponse(w, err.Code, err.AsMessage())
 	} else {
 		if r.Header.Get("Content-Type") == "application/xml" {
 			w.Header().Add("Content-Type", "application/xml")
 			xml.NewEncoder(w).Encode(customer)
 		} else {
-			w.Header().Add("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(customer)
+			writeResponse(w, http.StatusOK, customer)
 		}
+	}
+}
+
+func writeResponse(w http.ResponseWriter, code int, data interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		panic(err)
 	}
 }
